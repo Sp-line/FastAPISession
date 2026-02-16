610da223b75e6f7db7d2de9eda7b5686a1729119
spline <dmcemin@gmail.com>
2026-02-16 18:28:02 +0100

    rename: fastapi-application dir to app

--- app/core/config.py
+++ app/core/config.py
@@ -42,6 +42,7 @@ class Settings(BaseSettings):
         case_sensitive=False,
         env_nested_delimiter="__",
         env_prefix="APP_CONFIG__",
+        extra="ignore"
     )
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
