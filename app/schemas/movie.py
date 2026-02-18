e1b3fbeca9fa1d80ca024cfe5a67f64f6a6a8086
spline <dmcemin@gmail.com>
2026-02-18 21:52:05 +0100

    add: movie schemas

---
+++ app/schemas/movie.py
@@ -0,0 +1,46 @@
+from datetime import datetime
+from typing import Annotated
+
+from pydantic import BaseModel, Field, ConfigDict
+
+from constants import MovieLimits, ImageUrlLimits
+from constants.movie import AgeRating
+from schemas.base import Id
+
+
+class MovieBase(BaseModel):
+    title: Annotated[str, Field(min_length=MovieLimits.TITLE_MIN, max_length=MovieLimits.TITLE_MAX)]
+    duration: Annotated[int, Field(ge=MovieLimits.DURATION_MIN, le=MovieLimits.DURATION_MAX)]
+    age_rating: AgeRating
+    premiere_date: datetime | None = None
+    poster_url: Annotated[str, Field(min_length=ImageUrlLimits.MIN, max_length=ImageUrlLimits.MAX)]
+
+
+class MovieCreateDB(Id, MovieBase):
+    slug: Annotated[str, Field(min_length=MovieLimits.SLUG_MIN, max_length=MovieLimits.SLUG_MAX)]
+
+
+class MovieCreateReq(Id, MovieBase):
+    pass
+
+
+class MovieUpdateBase(BaseModel):
+    title: Annotated[str | None, Field(min_length=MovieLimits.TITLE_MIN, max_length=MovieLimits.TITLE_MAX)] = None
+    duration: Annotated[int | None, Field(ge=MovieLimits.DURATION_MIN, le=MovieLimits.DURATION_MAX)] = None
+    age_rating: AgeRating | None = None
+    premiere_date: datetime | None = None
+
+
+class MovieUpdateDB(MovieUpdateBase):
+    slug: Annotated[str | None, Field(min_length=MovieLimits.SLUG_MIN, max_length=MovieLimits.SLUG_MAX)] = None
+    poster_url: Annotated[str | None, Field(min_length=ImageUrlLimits.MIN, max_length=ImageUrlLimits.MAX)] = None
+
+
+class MovieUpdateReq(MovieUpdateBase):
+    pass
+
+
+class MovieRead(Id, MovieBase):
+    slug: Annotated[str, Field(min_length=MovieLimits.SLUG_MIN, max_length=MovieLimits.SLUG_MAX)]
+
+    model_config = ConfigDict(from_attributes=True)
