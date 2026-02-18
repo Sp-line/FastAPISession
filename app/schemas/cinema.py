ea19e86d5e6b67efb64b500e19a8b4ab9105f892
spline <dmcemin@gmail.com>
2026-02-18 21:48:23 +0100

    update: cinema schemas

--- app/schemas/cinema.py
+++ app/schemas/cinema.py
@@ -9,36 +9,30 @@ from schemas.base import Id
 
 class CinemaBase(BaseModel):
     name: Annotated[str, Field(min_length=CinemaLimits.NAME_MIN, max_length=CinemaLimits.NAME_MAX)]
-    slug: Annotated[str, Field(min_length=CinemaLimits.SLUG_MIN, max_length=CinemaLimits.SLUG_MAX)]
     description: str | None = None
     is_available: bool = True
 
 
 class CinemaCreateDB(CinemaBase):
+    slug: Annotated[str, Field(min_length=CinemaLimits.SLUG_MIN, max_length=CinemaLimits.SLUG_MAX)]
+
+
+class CinemaCreateReq(CinemaBase):
     pass
-
-
-class CinemaCreateReq(BaseModel):
-    name: Annotated[str, Field(min_length=CinemaLimits.NAME_MIN, max_length=CinemaLimits.NAME_MAX)]
-    description: str | None = None
-    is_available: bool = True
 
 
 class CinemaUpdateBase(BaseModel):
     name: Annotated[str | None, Field(min_length=CinemaLimits.NAME_MIN, max_length=CinemaLimits.NAME_MAX)] = None
-    slug: Annotated[str | None, Field(min_length=CinemaLimits.SLUG_MIN, max_length=CinemaLimits.SLUG_MAX)] = None
     description: str | None = None
     is_available: bool | None = None
 
 
 class CinemaUpdateDB(CinemaUpdateBase):
+    slug: Annotated[str | None, Field(min_length=CinemaLimits.SLUG_MIN, max_length=CinemaLimits.SLUG_MAX)] = None
+
+
+class CinemaUpdateReq(CinemaUpdateBase):
     pass
-
-
-class CinemaUpdateReq(BaseModel):
-    name: Annotated[str | None, Field(min_length=CinemaLimits.NAME_MIN, max_length=CinemaLimits.NAME_MAX)] = None
-    description: str | None = None
-    is_available: bool | None = None
 
 
 class CinemaRead(Id, CinemaBase):
