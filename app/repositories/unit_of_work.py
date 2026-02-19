7cebcd69d2cdc732b98b4378fd91a38ddf9ff67f
spline <dmcemin@gmail.com>
2026-02-19 13:27:22 +0100

    add: UnitOfWork

---
+++ app/repositories/unit_of_work.py
@@ -0,0 +1,22 @@
+from typing import Self
+
+from sqlalchemy.ext.asyncio import AsyncSession
+
+
+class UnitOfWork:
+    def __init__(self, session: AsyncSession) -> None:
+        self._session = session
+
+    async def __aenter__(self) -> Self:
+        return self
+
+    async def __aexit__(
+            self,
+            exc_type: object | None,
+            exc_val: BaseException | None,
+            exc_tb: object | None,
+    ) -> None:
+        if exc_type:
+            await self._session.rollback()
+        else:
+            await self._session.commit()
