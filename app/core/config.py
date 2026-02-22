4df05fee74458b555741231f28c6b4df70976794
spline <dmcemin@gmail.com>
2026-02-22 23:25:28 +0100

    add: FastStreamConfig, JStreamConfig, LoggingConfig

--- app/core/config.py
+++ app/core/config.py
@@ -1,14 +1,19 @@
-from pydantic import BaseModel
+import logging
+
+from nats.js.api import RetentionPolicy, DiscardPolicy
+from pydantic import BaseModel, NatsDsn
 from pydantic import PostgresDsn
 from pydantic_settings import (
     BaseSettings,
     SettingsConfigDict,
 )
 
+from log import LogLevel
+
 
 class RunConfig(BaseModel):
     host: str = "0.0.0.0"
-    port: int = 8000
+    port: int = 8001
 
 
 class ApiV1Prefix(BaseModel):
@@ -18,6 +23,28 @@ class ApiV1Prefix(BaseModel):
 class ApiPrefix(BaseModel):
     prefix: str = "/api"
     v1: ApiV1Prefix = ApiV1Prefix()
+
+
+class FastStreamConfig(BaseModel):
+    nats_url: NatsDsn
+
+
+class JStreamConfig(BaseModel):
+    name: str = "showtimes_stream"
+    subjects: list[str] = ["showtimes.>", ]
+    retention: RetentionPolicy = RetentionPolicy.LIMITS
+    max_age: int = 24 * 60 * 60
+    discard: DiscardPolicy = DiscardPolicy.OLD
+
+
+class LoggingConfig(BaseModel):
+    log_level: LogLevel = "info"
+    log_format: str = "[%(asctime)s.%(msecs)03d] %(module)10s:%(lineno)-3d %(levelname)-7s - %(message)s"
+    log_datefmt: str = "%Y-%m-%d %H:%M:%S"
+
+    @property
+    def log_level_value(self) -> int:
+        return logging.getLevelNamesMapping()[self.log_level.upper()]
 
 
 class DatabaseConfig(BaseModel):
@@ -44,9 +71,12 @@ class Settings(BaseSettings):
         env_prefix="APP_CONFIG__",
         extra="ignore"
     )
+    logging: LoggingConfig = LoggingConfig()
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
+    jstream: JStreamConfig = JStreamConfig()
+    faststream: FastStreamConfig
     db: DatabaseConfig
 
 
-settings = Settings() # type: ignore[call-arg]
+settings = Settings()  # type: ignore[call-arg]
