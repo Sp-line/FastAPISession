1d9d8e89019d92a4930f290b042c1d925ce19a35
spline <dmcemin@gmail.com>
2026-02-22 21:42:09 +0100

    add: excl_session_hall_time_overlap, ck_sessions_end_time_after_start_time ConstraintRules

--- app/integrity_handler/session.py
+++ app/integrity_handler/session.py
@@ -1,5 +1,6 @@
 from constants.db import PostgresErrorCode
-from exceptions.db import RelatedObjectNotFoundException, DeleteConstraintException
+from exceptions.db import RelatedObjectNotFoundException, DeleteConstraintException, ExclusionException, \
+    CheckConstraintException
 from integrity_handler.base import TableErrorHandler
 from schemas.db import ConstraintRule
 
@@ -30,7 +31,27 @@ fk_bookings_session_id_sessions_delete = ConstraintRule(
     )
 )
 
+excl_session_hall_time_overlap = ConstraintRule(
+    name="excl_session_hall_time_overlap",
+    error_code=PostgresErrorCode.EXCLUSION_VIOLATION,
+    exception=ExclusionException(
+        "sessions",
+        "start_time", "end_time"
+    )
+)
+
+ck_sessions_end_time_after_start_time = ConstraintRule(
+    name="ck_sessions_end_time_after_start_time",
+    error_code=PostgresErrorCode.CHECK_VIOLATION,
+    exception=CheckConstraintException(
+        table_name="sessions",
+        expression="end_time > start_time"
+    )
+)
+
 session_error_handler = TableErrorHandler(
+    ck_sessions_end_time_after_start_time,
+    excl_session_hall_time_overlap,
     fk_sessions_hall_id_halls,
     fk_sessions_movie_id_movies,
     fk_bookings_session_id_sessions_delete
