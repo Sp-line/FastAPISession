b450e7f379b7be174708334a6268e271da6d4827
spline <dmcemin@gmail.com>
2026-02-22 23:30:16 +0100

    add: setup_faststream_dishka & app.include_router(fs_router) to create app func

--- app/create_app.py
+++ app/create_app.py
@@ -2,9 +2,12 @@ from contextlib import asynccontextmanager
 
 from dishka import make_async_container
 from dishka.integrations.fastapi import setup_dishka as setup_fastapi_dishka
+from dishka.integrations.faststream import setup_dishka as setup_faststream_dishka
 from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
 
+import event_handlers  # noqa: F401
+from core import fs_router
 from core.models import db_helper
 from dependencies import InfrastructureProvider, RepositoryProvider, ServiceProvider
 from exceptions import register_exception_handlers
@@ -31,7 +34,9 @@ def create() -> FastAPI:
     )
 
     setup_fastapi_dishka(container, app)
+    setup_faststream_dishka(container, broker=fs_router.broker, auto_inject=True)
 
+    app.include_router(fs_router)
     register_exception_handlers(app)
 
     return app
