e0a8a9e8f4d0124075c1d570980d7ea564cb402d
spline <dmcemin@gmail.com>
2026-02-24 00:32:11 +0100

    add: recalculate_and_update_capacity method for HallRepository

--- app/repositories/hall.py
+++ app/repositories/hall.py
@@ -1,6 +1,7 @@
+from sqlalchemy import select, func, update
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from core.models import Hall
+from core.models import Hall, Seat
 from integrity_handler import hall_error_handler
 from repositories.base import RepositoryBase
 from schemas.hall import HallCreateDB, HallUpdateDB
@@ -20,3 +21,22 @@ class HallRepository(
             session=session,
             table_error_handler=hall_error_handler,
         )
+
+    async def recalculate_and_update_capacity(self, *hall_ids: int) -> None:
+        if not hall_ids:
+            return
+
+        count_subquery = (
+            select(func.count(Seat.id))
+            .where(Seat.hall_id == Hall.id)
+            .correlate(Hall)
+            .scalar_subquery()
+        )
+
+        stmt = (
+            update(Hall)
+            .where(Hall.id.in_(hall_ids))
+            .values(capacity=count_subquery)
+        )
+
+        await self._session.execute(stmt)
