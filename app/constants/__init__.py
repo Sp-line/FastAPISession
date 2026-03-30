886ab61915512bc828b73c1360cf1e6c08b80633
spline <dmcemin@gmail.com>
2026-03-30 15:38:59 +0200

    add: TicketStatus import

--- app/constants/__init__.py
+++ app/constants/__init__.py
@@ -12,16 +12,18 @@ __all__ = (
     "ImageUrlLimits",
     "MovieLimits",
     "SessionLimits",
-    "BookingLimits"
+    "BookingLimits",
+    "TicketStatus"
 )
 
 from constants.address import AddressLimits
 from constants.base import ImageUrlLimits
+from constants.booking import BookingStatus, BookingLimits
 from constants.cinema import CinemaLimits
 from constants.hall import HallLimits, HallTechType
 from constants.movie import MovieLimits
 from constants.seat import SeatLimits
-from constants.booking import BookingStatus, BookingLimits
 from constants.seat_type import SeatType
 from constants.session import DimensionFormat, ScreenTechnology, SessionLimits
 from constants.session_price import SessionPriceLimits
+from constants.ticket import TicketStatus
