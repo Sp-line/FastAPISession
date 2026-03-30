06f3611a972e5cd17543d2e8293b7de6a23581e8
spline <dmcemin@gmail.com>
2026-03-30 20:57:27 +0200

    use: AsyncGenerator return type for get_db_session

--- app/dependencies/infrastructure.py
+++ app/dependencies/infrastructure.py
@@ -1,4 +1,4 @@
-from typing import AsyncIterable
+from collections.abc import AsyncGenerator
 
 from dishka import Provider, Scope, provide, alias
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -11,6 +11,6 @@ class InfrastructureProvider(Provider):
     session_alias = alias(source=EventSession, provides=AsyncSession)
 
     @provide(scope=Scope.REQUEST)
-    async def get_db_session(self) -> AsyncIterable[EventSession]:
+    async def get_db_session(self) -> AsyncGenerator[EventSession, None]:
         async with db_helper.session_factory() as session:
             yield session
